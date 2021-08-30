04f652c2e9bbfc582df2ceaee612e3867daf5e3f
ind-spirit <error156@yandex.ru>
2021-08-31 03:56:35 +0700

    complete site without animations(?)

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,28 @@
-// window.onload = () => {
-//     console.log('script');
-//   setTimeout(function () {
-//     document.body.style.backgroundColor = "blue";
-//   }, 3000);
-// };
+window.onload = function () {
+    console.log("script");
+    var timer = document.getElementsByClassName("fifth-screen__timer")[0];
+    console.log(timer);
+    var string = timer.firstChild.toString();
+    console.log(timer.firstChild);
+    console.log(string);
+    console.log(parseInt(string));
+    var min = 29;
+    var sec = 0;
+    timer.innerHTML = min + ":" + sec;
+    function mins() {
+        min--;
+        timer.innerHTML = min + ":" + sec;
+    }
+    function secs() {
+        if (!(sec == 0)) {
+            sec--;
+            timer.innerHTML = min + ":" + sec;
+        }
+        else {
+            sec = 59;
+            timer.innerHTML = min + ":" + sec;
+        }
+    }
+    var intervalMin = window.setInterval(mins, 60000);
+    var intervalSec = window.setInterval(secs, 1000);
+};
